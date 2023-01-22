a84e1386b9322ba15492a9297746ec1e5b70e71f
jenifergs <contato.comercialjenifer@gmail.com>
2023-01-22 16:46:23 -0300

    feat: Rota para a proposta

---
+++ src/routes/proposta.routes.ts
@@ -0,0 +1,16 @@
+import modalidadeTarifaria from '../middlewares/modalidadeTarifaria';
+import NumeroDoDocumento from '../middlewares/numeroDoDocumento';
+import validarHistoricoDeConsumo from '../middlewares/validarHistoricoDeConsumo';
+import TiposDeConexao from '../middlewares/tiposDeConexao';
+import validacaoClasseConsumo from '../middlewares/validacaoClasseConsumo';
+import { Router } from 'express';
+import PropostaController from '../controller/PropostaController';
+// precisa importar as rotas aqui
+
+
+const propostaRouter = Router();
+const propostaCliente = new PropostaController();
+
+propostaRouter.post('/proposta', modalidadeTarifaria, NumeroDoDocumento, validarHistoricoDeConsumo, TiposDeConexao, validacaoClasseConsumo, propostaCliente.novaProposta);
+
+export default propostaRouter;
